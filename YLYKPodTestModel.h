c6397f21e300d9bdf52ee8b30c807623d1275496
xufeng <35591166+xufengbj@users.noreply.github.com>
2018-01-19 17:28:00 +0800

    podTest
    上传到cocoaPod 测试文件

---
+++ YLYKPodTestModel.h
@@ -0,0 +1,15 @@
+//
+//  YLYKPodTestModel.h
+//  suanfa
+//
+//  Created by 许锋 on 2018/1/19.
+//  Copyright © 2018年 许锋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YLYKPodTestModel : NSObject
+
+- (NSString *)getCreateAuthor;
+
+@end
---
+++ YLYKPodTestModel.m
@@ -0,0 +1,16 @@
+//
+//  YLYKPodTestModel.m
+//  suanfa
+//
+//  Created by 许锋 on 2018/1/19.
+//  Copyright © 2018年 许锋. All rights reserved.
+//
+
+#import "YLYKPodTestModel.h"
+
+@implementation YLYKPodTestModel
+- (NSString *)getCreateAuthor {
+    return @"创建作者许锋";
+}
+
+@end
